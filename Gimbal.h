65d50a504c783fa6ae2d613e3667a4fb4acd6419
bdavis2020 <bbddwv@umsystem.edu>
2022-02-19 15:08:09 -0600

    Pushed Science Gripper code

---
+++ Gimbal.h
@@ -0,0 +1,13 @@
+#ifndef GIMBAL_H
+#define GIMBAL_H
+
+#include "RoveComm.h"
+#include <Servo.h>
+
+#define SERVO 1
+
+#define SERVO_MIN 0
+#define SERVO_MAX 180
+#define ROVECOMM_DELAY 10
+
+#endif
---
+++ Gimbal.ino
@@ -0,0 +1,39 @@
+#include "Gimbal.h"
+
+Servo scoop;
+int16_t servoPosition;
+
+EthernetServer TCPServer(RC_ROVECOMM_GIMBALBOARD_PORT);
+RoveCommEthernet RoveComm;
+rovecomm_packet packet;
+
+void setup()
+{
+  Serial.begin(115000);
+
+  delay(ROVECOMM_DELAY);
+  RoveComm.begin(RC_GIMBALBOARD_FOURTHOCTET, &TCPServer);
+  delay(ROVECOMM_DELAY);
+
+  scoop.attach(SERVO);
+  scoop.write(SERVO_MIN);
+}
+
+void loop()
+{
+  packet = RoveComm.read();
+  Serial.println(packet.data_id);
+
+  switch(packet.data_id)
+  {
+    case RC_GIMBALBOARD_LEFTDRIVEGIMBALINCREMENT_DATA_ID:
+      int16_t* position = (int16_t*)packet.data;
+      Serial.println("Incremental: ");
+      Serial.println(position[0]);
+      Serial.println(position[1]);
+      scoop.write(position);
+      break;
+    default:
+      break;
+  }
+}
