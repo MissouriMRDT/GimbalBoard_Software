e34f6f0393ded1ae66d96a7e6fb92412bc2b2821
thesprintingturtle <robert.squires01@gmail.com>
2022-02-25 20:34:49 -0600

    She works

--- Gimbal/Gimbal.h
+++ Gimbal/Gimbal.h
@@ -4,10 +4,42 @@
 #include "RoveComm.h"
 #include <Servo.h>
 
-#define SERVO 1
+#define LMP_SERVO 1
+#define LMT_SERVO 2
+#define RMP_SERVO 3
+#define RMT_SERVO 4
+#define LD_SERVO 5
+#define SPARE1 6
+#define RD_SERVO 7
+#define SPARE2 8
 
-#define SERVO_MIN 0
-#define SERVO_MAX 180
+Servo leftMainPan;
+Servo leftMainTilt;
+Servo rightMainPan;
+Servo rightMainTilt;
+Servo leftDrive;
+Servo spare1;
+Servo rightDrive;
+Servo spare2;
+
+
+#define LMP_SERVO_MIN 0
+#define LMP_SERVO_MAX 180
+#define LMT_SERVO_MIN 0
+#define LMT_SERVO_MAX 180
+#define RMP_SERVO_MIN 0
+#define RMP_SERVO_MAX 180
+#define RMT_SERVO_MIN 0
+#define RMT_SERVO_MAX 180
+#define LD_SERVO_MIN 0
+#define LD_SERVO_MAX 180
+#define SPARE1_MIN 0
+#define SPARE1_MAX 180
+#define RD_SERVO_MIN 0
+#define RD_SERVO_MAX 180
+#define SPARE2_MIN 0
+#define SPARE2_MAX 180
+
 #define ROVECOMM_DELAY 10
 
 #endif
--- Gimbal/Gimbal.ino
+++ Gimbal/Gimbal.ino
@@ -1,11 +1,9 @@
 #include "Gimbal.h"
 
-Servo scoop;
-int16_t servoPosition;
-
 EthernetServer TCPServer(RC_ROVECOMM_GIMBALBOARD_PORT);
 RoveCommEthernet RoveComm;
 rovecomm_packet packet;
+int16_t* position;
 
 void setup()
 {
@@ -15,23 +13,60 @@ void setup()
   RoveComm.begin(RC_GIMBALBOARD_FOURTHOCTET, &TCPServer);
   delay(ROVECOMM_DELAY);
 
-  scoop.attach(SERVO);
-  scoop.write(SERVO_MIN);
+  leftMainPan.attach(LMP_SERVO);
+  leftMainPan.write(LMP_SERVO_MIN);
+  leftMainTilt.attach(LMT_SERVO);
+  leftMainTilt.write(LMT_SERVO_MIN);
+  rightMainPan.attach(RMP_SERVO);
+  rightMainPan.write(RMP_SERVO_MIN);
+  rightMainTilt.attach(RMT_SERVO);
+  rightMainTilt.write(RMT_SERVO_MIN);
+  leftDrive.attach(LD_SERVO);
+  leftDrive.write(LD_SERVO_MIN);
+  spare1.attach(SPARE1);
+  spare1.write(SPARE1_MIN);
+  rightDrive.attach(RD_SERVO);
+  rightDrive.write(RD_SERVO_MIN);
+  spare2.attach(SPARE2);
+  spare2.attach(SPARE2_MIN);
 }
 
 void loop()
 {
   packet = RoveComm.read();
-  Serial.println(packet.data_id);
+  //Serial.println(packet.data_id);
 
   switch(packet.data_id)
   {
     case RC_GIMBALBOARD_LEFTDRIVEGIMBALINCREMENT_DATA_ID:
-      int16_t* position = (int16_t*)packet.data;
+      position = (int16_t*)packet.data;
       Serial.println("Incremental: ");
       Serial.println(position[0]);
       Serial.println(position[1]);
-      scoop.write(position[0]);
+      leftDrive.write(position[0]);
+      break;
+    case RC_GIMBALBOARD_RIGHTDRIVEGIMBALINCREMENT_DATA_ID:
+      position = (int16_t*)packet.data;
+      // Serial.println("Incremental: ");
+      // Serial.println(position[0]);
+      // Serial.println(position[1]);
+      rightDrive.write(position[0]);
+      break;
+    case RC_GIMBALBOARD_LEFTMAINGIMBALINCREMENT_DATA_ID:
+      position = (int16_t*)packet.data;
+      // Serial.println("Incremental: ");
+      // Serial.println(position[0]);
+      // Serial.println(position[1]);
+      leftMainPan.write(position[0]);
+      leftMainTilt.write(position[1]);
+      break;
+    case RC_GIMBALBOARD_RIGHTMAINGIMBALINCREMENT_DATA_ID:
+      position = (int16_t*)packet.data;
+      // Serial.println("Incremental: ");
+      // Serial.println(position[0]);
+      // Serial.println(position[1]);
+      rightMainPan.write(position[0]);
+      rightMainTilt.write(position[1]);
       break;
     default:
       break;
