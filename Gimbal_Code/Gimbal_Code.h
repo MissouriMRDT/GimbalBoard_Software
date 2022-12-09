e8d09bdd7c608e4592efe9f02871bbf0698b6e5b
TylerNguyen03 <92827445+TylerNguyen03@users.noreply.github.com>
2022-12-08 18:26:49 -0600

    defined min, max, and start postions

--- Gimbal_Code/Gimbal_Code.h
+++ Gimbal_Code/Gimbal_Code.h
@@ -12,4 +12,28 @@
 #define L_Tilt_Servo 6
 #define Extra_Servo 33
 
+//Starting Postions
+#define Servo1_START 135
+#define Servo2_START 135
+#define Servo3_START 135
+#define Servo4_START 135
+#define Servo5_START 135
+#define Servo6_START 135
+
+//Minimum and Maximum Servo Values
+#define R_Drive_Servo_Min 0
+#define R_Drive_Servo_Max 180
+#define R_Pan_Servo_Min 0
+#define R_Pan_Servo_Max 180
+#define R_Tilt_Servo_Min 0
+#define R_Tilt_Servo_Max 180
+#define L_Drive_Servo_Min 0
+#define L_Drive_Servo_Max 180
+#define L_Pan_Servo_Min 0
+#define L_Pan_Servo_Max 180
+#define L_Tilt_Servo_Min 0
+#define L_Tilt_Servo_Max 180
+#define Extra_Servo_Min 0
+#define Extra_Servo_Min 180
+
 #endif
---
+++ Gimbal_Code/Gimbal_Code.ino
@@ -0,0 +1,22 @@
+#include Gimbal_Code_h
+
+void setup()
+{
+    //Attach Servo Array to Respective Pins
+    servos[0].attach(R_Drive_Servo);
+    servos[1].attach(R_Pan_Servo);
+    servos[2].attach(R_Tilt_Servo);
+    servos[3].attach(L_Drive_Servo);
+    servos[4].attach(L_Pan_Servo);
+    servos[5].attach(L_Tilt_Servo);
+    servos[6].attach(Extra_Servo);
+
+    //Servo Postions Set to Starting Positons
+    Servo_Start[0] = Servo1_START;
+    Servo_Start[1] = Servo2_START;
+    Servo_Start[2] = Servo3_START;
+    Servo_Start[3] = Servo4_START;
+    Servo_Start[4] = Servo5_START;
+    Servo_Start[5] = Servo6_START;
+    Servo_Start[6] = Extra_Servo;
+}
